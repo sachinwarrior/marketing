a726a8660de090678e71d2ab6d4f020837b56ce6
Trang Hoang <trang@warriormade.com>
2021-03-08 19:20:08 +0000

    ad_id in fb_sales has to be not null

--- models/facebook/fb_sales_spend.sql
+++ models/facebook/fb_sales_spend.sql
@@ -3,5 +3,5 @@
 )}}
 select * 
 from (select * except(date_start), cast(date_start as date) as date_start from {{ref('fb_ads')}}) facebook_ads
-full outer join {{ref('fb_sales_90d_agg_v3')}} fb_sales
+full outer join (select * from {{ref('fb_sales_90d_agg_v3')}} where ad_id is not null) fb_sales
 on fb_sales.order_date = facebook_ads.date_start and fb_sales.ad_id = facebook_ads.fb_ad_id
