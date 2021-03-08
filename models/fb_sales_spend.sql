8672f0b658125d9797f49639dde195444db9dec2
Trang Hoang <trang@warriormade.com>
2021-03-08 16:58:23 +0000

    add fb_sales w ads

--- models/fb_sales_spend.sql
+++
@@ -1,4 +0,0 @@
-select * 
-from (select * except(date_start), cast(date_start as date) as date_start from {{ref('fb_ads')}}) facebook_ads
-full outer join {{ ref('fb_sales_90d_agg_v3')}} fb_sales
-on fb_sales.order_date = facebook_ads.date_start and fb_sales.ad_id = facebook_ads.fb_ad_id
