d4902ea89de677b50bbf0abf057fc39c5d919729
Trang Hoang <trang@warriormade.com>
2021-03-19 19:28:09 +0000

    adding number_orders column

--- models/customer_info/users_pivot_kon.sql
+++ models/customer_info/users_pivot_kon.sql
@@ -41,14 +41,21 @@ LEFT JOIN (select customer_id, min(measurement_date) as trial_date_start, date_a
 on cast(customers.customer_id as string) = subscription_date.customer_id)),
 
 
- membership_revenue as (select sales.customer_id, sales_member_revenue - refund_member_revenue as member_revenue, 0 as ecommerce_revenue  
-FROM (select customer_id, SUM(total_amount) as sales_member_revenue from (select *
+membership_revenue as (select sales.customer_id, 
+  case when refund_member_revenue is null then sales_member_revenue 
+  else sales_member_revenue - refund_member_revenue 
+  end as member_revenue, 
+  0 as ecommerce_revenue,
+  case when refund_transaction is null then complete_transaction 
+  else complete_transaction - refund_transaction 
+  end as orders
+FROM (select customer_id, SUM(total_amount) as sales_member_revenue, count(transaction_id) as complete_transaction from (select *
   from konnektive.transactions_cleaned 
   where status != '' and status != '0' and status is not null)
 where txn_type = 'SALE' AND response_type = 'SUCCESS'
 group by customer_id) sales
 
-LEFT JOIN (select customer_id, SUM(total_amount) as refund_member_revenue from (select *
+LEFT JOIN (select customer_id, SUM(total_amount) as refund_member_revenue, count(transaction_id) as refund_transaction from (select *
   from konnektive.transactions_cleaned 
   where status != '' and status != '0' and status is not null)
 where txn_type = 'REFUND' AND response_type = 'SUCCESS'
@@ -56,14 +63,21 @@ group by customer_id) refund
 ON sales.customer_id = refund.customer_id),
 
 
-non_membership_revenue  as (select sales.customer_id,  0 as member_revenue, sales_ecommerce_revenue - refund_ecommerce_revenue as ecommerce_revenue, 
-FROM (select customer_id, SUM(total_amount) as sales_ecommerce_revenue from (select *
+non_membership_revenue  as (select sales.customer_id,  
+  0 as member_revenue, 
+  case when refund_ecommerce_revenue is null then sales_ecommerce_revenue
+  else sales_ecommerce_revenue - refund_ecommerce_revenue 
+  end as ecommerce_revenue, 
+  case when refund_transaction is null then complete_transaction 
+  else complete_transaction - refund_transaction 
+  end as orders
+FROM (select customer_id, SUM(total_amount) as sales_ecommerce_revenue, count(transaction_id) as complete_transaction from (select *
   from konnektive.transactions_cleaned 
   where status = '' or status = '0' or status is null)
 where txn_type = 'SALE' AND response_type = 'SUCCESS'
 group by customer_id) sales
 
-LEFT JOIN (select customer_id, SUM(total_amount) as refund_ecommerce_revenue from (select *
+LEFT JOIN (select customer_id, SUM(total_amount) as refund_ecommerce_revenue, count(transaction_id) as refund_transaction from (select *
   from konnektive.transactions_cleaned 
   where status = '' or status = '0' or status is null)
 where txn_type = 'REFUND' AND response_type = 'SUCCESS'
@@ -72,16 +86,19 @@ ON sales.customer_id = refund.customer_id),
 
 
 revenue as (select customer_id, sum(member_revenue) as member_revenue, 
-sum(ecommerce_revenue) as ecommerce_revenue
+sum(ecommerce_revenue) as ecommerce_revenue, 
+sum(orders) as number_orders
 from 
 (select customer_id,
         case when member_revenue is null then 0 else member_revenue end member_revenue, 
         case when ecommerce_revenue is null then 0 else ecommerce_revenue end ecommerce_revenue,
+        orders
 from non_membership_revenue
 union all 
 select customer_id,
         case when member_revenue is null then 0 else member_revenue end member_revenue, 
         case when ecommerce_revenue is null then 0 else ecommerce_revenue end ecommerce_revenue,
+        orders
 from membership_revenue)
 group by customer_id),
 
