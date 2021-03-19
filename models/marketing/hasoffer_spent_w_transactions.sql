56aa897f1666ba154d76bbe35f3f02c99aa76afd
Trang Hoang <trang@warriormade.com>
2021-03-19 17:40:06 +0000

    fix pt2

--- models/marketing/hasoffer_spent_w_transactions.sql
+++ models/marketing/hasoffer_spent_w_transactions.sql
@@ -15,16 +15,22 @@ with jb_woo as (select
 spend as (select 
         date as ad_date, 
         affiliate_name, affiliate_id, 
+        funnel_campaign,
         campaign_name,
         sum(clicks) as clicks, 
         sum(conversions) as conversions, 
         string_agg(offer_id,',') as offer_id, 
         sum(payout) as payout 
-    from hasoffers.spend_clean_aggregated
-    group by date, affiliate_name, affiliate_id, campaign_name)
+    from (select *, case when campaign_name = '3' or campaign_name ='25' then 'abs'
+        when campaign_name = '27' then 'keto carbs'
+        when campaign_name = '14' then 'sweet treats'
+        when campaign_name = '17' then 'go2protein'
+        end funnel_campaign
+         from hasoffers.spend_clean_aggregated)
+    group by date, affiliate_name, affiliate_id, funnel_campaign, campaign_name)
 
 select *
 from spend
 left join jb_woo 
-on jb_woo.date = spend.ad_date and jb_woo.mktg_custom_1 = spend.affiliate_name 
+on jb_woo.date = spend.ad_date and jb_woo.mktg_custom_1 = spend.affiliate_name and jb_woo.funnel= spend.funnel_campaign 
 where mktg_custom_1 is not null
