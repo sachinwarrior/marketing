2dd7e5e3ea3f48f90fba7d5a8c569c624049516b
Trang Hoang <trang@warriormade.com>
2021-03-25 16:14:44 +0000

    adding fb_ad_id is not null

--- models/reports/fb_sales_spend.sql
+++ models/reports/fb_sales_spend.sql
@@ -1,7 +1,9 @@
 {{ config (
     materialized="table"
 )}}
-select * 
+with fb_sales_spend as (select * 
 from (select * except(date_start), cast(date_start as date) as date_start from {{ref('fb_ads')}}) facebook_ads
 full outer join (select * from {{ref('fb_sales_90d_agg_v3')}} where ad_id is not null) fb_sales
-on fb_sales.order_date = facebook_ads.date_start and fb_sales.ad_id = facebook_ads.fb_ad_id
+on fb_sales.order_date = facebook_ads.date_start and fb_sales.ad_id = facebook_ads.fb_ad_id)
+
+select * from fb_sales_spend where fb_ad_id is not null and ad_id is not null order by date_start desc
