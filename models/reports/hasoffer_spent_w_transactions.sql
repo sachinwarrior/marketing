1002e467995f9cbfee7e844e32cac33be6f01e3c
Trang Hoang <trang@warriormade.com>
2021-04-01 18:37:00 +0000

    again fix hasoffer

--- models/reports/hasoffer_spent_w_transactions.sql
+++ models/reports/hasoffer_spent_w_transactions.sql
@@ -1,7 +1,6 @@
 {{ config (
     materialized="table"
 )}}
-
 with jb_woo as (select 
         date,
         mktg_custom_1,
@@ -10,26 +9,36 @@ with jb_woo as (select
         sum(trials) as trials, 
         sum(sales_count) as sales_count, 
         sum(cogs) as cogs 
-    from {{ ref('jb_woo_aggregated') }} 
+    from {{ref('jb_woo_aggregated')}}
     group by date, funnel, mktg_custom_1), 
 
 spend as (select 
         date as ad_date, 
         affiliate_name, affiliate_id, 
-        case when campaign_name = '3' or campaign_name ='25' then 'abs'
+        funnel_campaign,
+        string_agg(campaign_name,',') as campaign_name,
+        sum(clicks) as clicks , 
+        sum(conversions) as conversions, 
+        string_agg(offer_id,',') as offer_id, 
+       sum(payout) as payout
+    from (select *,  case when campaign_name = '3' or campaign_name ='25' then 'abs'
         when campaign_name = '27' then 'keto carbs'
         when campaign_name = '14' then 'keto sweet'
         when campaign_name = '17' then 'go2protein'
         end funnel_campaign,
-        campaign_name,
-        clicks, 
-        conversions, 
-        offer_id, 
-       payout 
-    from {{ref('spend_clean_aggregated')}})
+        from {{ref('spend_clean_aggregated')}} )
+    group by date, affiliate_name, affiliate_id, 
+        funnel_campaign),
 
-select *
+hasoffer_spent as (select *
 from spend
 left join jb_woo 
-on jb_woo.date = spend.ad_date and jb_woo.mktg_custom_1 = spend.affiliate_name and jb_woo.funnel= spend.funnel_campaign 
-where mktg_custom_1 is not null
+on jb_woo.date = spend.ad_date and jb_woo.mktg_custom_1 = spend.affiliate_name and jb_woo.funnel= spend.funnel_campaign )
+
+select * except(date, mktg_custom_1, funnel, sales, trials, sales_count, cogs), 
+  case when sales is null then 0 else sales end sales, 
+  case when trials is null then 0 else trials end trials,
+  case when sales_count is null then 0 else sales_count end sales_count, 
+  case when cogs is null then 0 else cogs end cogs,
+from hasoffer_spent
+where affiliate_name is not null
