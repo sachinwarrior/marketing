4d3a1990e82a1b260e8f2d34fcf437db36e83198
Trang Hoang <trang@warriormade.com>
2021-07-21 19:58:25 +0000

    changing  from and to or in fb_ad_id is not null and ad_id is not null

--- models/reports/fb_sales_spend.sql
+++ models/reports/fb_sales_spend.sql
@@ -6,4 +6,4 @@ from (select * except(date_start), cast(date_start as date) as date_start from {
 full outer join (select * from {{ref('fb_sales_90d_agg_v3')}} where ad_id is not null) fb_sales
 on fb_sales.order_date = facebook_ads.date_start and fb_sales.ad_id = facebook_ads.fb_ad_id)
 
-select * from fb_sales_spend where fb_ad_id is not null and ad_id is not null order by date_start desc
+select * from fb_sales_spend where fb_ad_id is not null or ad_id is not null order by date_start desc
